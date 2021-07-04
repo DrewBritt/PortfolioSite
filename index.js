5c465b2b3d073381abb90f0d43f6c041df035ffc
Drew Britt <drewbritt02@gmail.com>
2021-07-04 18:07:27 -0500

    Lock scrolling body when mobile navbar is active

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function toggleSideMenu() {
+    //Get mobile navbar element and set active
     let mobilenav = document.querySelector('.mobile-navbar');
     mobilenav.classList.toggle('active');
+
+    //Disable scrolling of body when navbar is active
+    document.body.classList.toggle('scroll-lock');
 }
